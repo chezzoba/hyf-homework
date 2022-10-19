552687782b523fbcf403beacafcca69420fd6b69
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2022-10-19 15:40:12 +0200

    Added Age-ify (A future age calculator)

---
+++ javascript/javascript1/week1/script.js
@@ -0,0 +1,8 @@
+// Age-ify (A future age calculator)
+let yearOfBirth = 1985;
+let yearFuture = 2025;
+
+// Used const as formula is fixed.
+const age = yearFuture - yearOfBirth;
+
+console.log(`You will be ${age} years old in ${yearFuture}`);
