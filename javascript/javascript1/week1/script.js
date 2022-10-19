1fd2f22233421ad82645c48d01b8359a7a99158c
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2022-10-19 16:03:46 +0200

    Added Housey pricey (A house price estimator)

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -25,4 +25,43 @@
         console.log (`Your dog will be ${dogYear} human years old in ${dogYearFuture}`);
     }
 
+// Housey pricey (A house price estimator)
+    let detailsOfHouse = [
+        {
+            name: "Peter", 
+            length: 10,
+            breadth: 8,
+            height: 10,  
+            gardenSizeInM2: 100,    
+            houseCost: 2500000,
+        },
+        {
+            name: "Julia", 
+            length: 11,
+            breadth: 5,
+            height: 8,  
+            gardenSizeInM2: 70,    
+            houseCost: 1000000,
+        },
+    ]
+    
+    // Using index number to access details of "Peter" or "Julia"
+    let indexNumber = 0;
+    
+    // Calculation volumeInMeters = length * breadth * height;
+    const volumeInMeters = (detailsOfHouse[indexNumber].length) * (detailsOfHouse[indexNumber].breadth) * (detailsOfHouse[indexNumber].height);
+    
+    // Calculation const housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+    const housePrice = volumeInMeters * 2.5 * 1000 + (detailsOfHouse[indexNumber].gardenSizeInM2) * 300;
+    console.log(housePrice);
+    
+    //Condition based on comparision between housePrice and houseCost
+    if(housePrice<(detailsOfHouse[indexNumber].houseCost)){
+        console.log(`${(detailsOfHouse[indexNumber].name)} is paying too much for the house`)
+    }else if (housePrice>(detailsOfHouse[indexNumber].houseCost)){
+        console.log(`${(detailsOfHouse[indexNumber].name)} is paying too little for the house`)
+    } else {
+        console.log(`${(detailsOfHouse[indexNumber].name)} is paying exact amount for the house`)
+    }
+
 
