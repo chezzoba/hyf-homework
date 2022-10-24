ce5a5ac396e5604d8943658fb9ac82eca436fde8
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2022-10-24 12:37:51 +0200

    Rewrote the code to calculate Peters and Julias House Price separately.

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -50,26 +50,41 @@
             houseCost: 1000000,
         },
     ]
+
+    // Calculation  Peters House volumeInMeters = length * breadth * height;
+    const volumeInMetersPetersHouse = (detailsOfHouse[0].length) * (detailsOfHouse[0].breadth) * (detailsOfHouse[0].height);
     
-    // Using index number to access details of "Peter" or "Julia"
-    let indexNumber = 0;
-    
-    // Calculation volumeInMeters = length * breadth * height;
-    const volumeInMeters = (detailsOfHouse[indexNumber].length) * (detailsOfHouse[indexNumber].breadth) * (detailsOfHouse[indexNumber].height);
-    
-    // Calculation const housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
-    const housePrice = volumeInMeters * 2.5 * 1000 + (detailsOfHouse[indexNumber].gardenSizeInM2) * 300;
-    console.log(housePrice);
+    // Calculation const housePrice = volumeInMetersPetersHouse * 2.5 * 1000 + gardenSizeInM2 * 300;
+    const housePricePetersHouse = volumeInMetersPetersHouse * 2.5 * 1000 + (detailsOfHouse[0].gardenSizeInM2) * 300;
+    console.log(housePricePetersHouse);
     
     //Condition based on comparision between housePrice and houseCost
-    if(housePrice<(detailsOfHouse[indexNumber].houseCost)){
-        console.log(`${(detailsOfHouse[indexNumber].name)} is paying too much for the house`)
-    }else if (housePrice>(detailsOfHouse[indexNumber].houseCost)){
-        console.log(`${(detailsOfHouse[indexNumber].name)} is paying too little for the house`)
+    if(housePricePetersHouse<(detailsOfHouse[0].houseCost)){
+        console.log(`${(detailsOfHouse[0].name)} is paying too much for the house`)
+    }else if (housePricePetersHouse>(detailsOfHouse[0].houseCost)){
+        console.log(`${(detailsOfHouse[0].name)} is paying too little for the house`)
     } else {
-        console.log(`${(detailsOfHouse[indexNumber].name)} is paying exact amount for the house`)
+        console.log(`${(detailsOfHouse[0].name)} is paying exact amount for the house`)
     }
 
+    // Calculation  Julias House volumeInMeters = length * breadth * height;
+    //const volumeInMetersJuliasHouse = (detailsOfHouse[1].length) * (detailsOfHouse[1].breadth) * (detailsOfHouse[1].height);
+    const volumeInMetersJuliasHouse = (detailsOfHouse[1].length) * (detailsOfHouse[1].breadth) * (detailsOfHouse[1].height);
+    
+    // Calculation const housePrice = volumeInMetersJuliasHouse * 2.5 * 1000 + gardenSizeInM2 * 300;
+    const housePriceJuliasHouse = volumeInMetersJuliasHouse * 2.5 * 1000 + (detailsOfHouse[1].gardenSizeInM2) * 300;
+    console.log(housePriceJuliasHouse);
+    
+    //Condition based on comparision between housePrice and houseCost
+    if(volumeInMetersJuliasHouse<(detailsOfHouse[1].houseCost)){
+        console.log(`${(detailsOfHouse[1].name)} is paying too much for the house`)
+    }else if (volumeInMetersJuliasHouse>(detailsOfHouse[1].houseCost)){
+        console.log(`${(detailsOfHouse[1].name)} is paying too little for the house`)
+    } else {
+        console.log(`${(detailsOfHouse[1].name)} is paying exact amount for the house`)
+    }
+
+    
 // Ez Namey (Startup name generator)
 
     //Create two arrays called firstWords, secondWords. 
